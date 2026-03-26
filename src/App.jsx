4e2cdbfa885b3d1b6719a895aa268ffb3f20659e
Rajdeep Chakraborty <rajdeepchka@gmail.com>
2026-03-26 19:09:43 +0530

    index css and setup complete

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+import FirstVideo from './sections/FirstVideo'
+import Jason from './sections/Jason'
+import SecondVideo from './sections/SecondVideo'
+import Lucia from './sections/Lucia'
+import PostCard from './sections/PostCard'
+import Final from './sections/Final'
+import Outro from './sections/Outro'
+import './App.css'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+
+      <FirstVideo />
+      <Jason />
+
+      <SecondVideo />
+      <Lucia />
+
+      <PostCard />
+      <Final />
+      <Outro />
+    </main>
+  )
+}
+
+export default App
