8c7a03a779bf7995c2ba77ea98898c5b2e6e8530
Rajdeep Chakraborty <rajdeepchka@gmail.com>
2026-03-31 23:52:33 +0530

    Adjusted: CSS for large screens
    - Large screen responsive
    - medium and below might crash-out

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,24 +8,24 @@ const Hero = () => {
 
     const { initialMaskPos, initialMaskSize, maskPos, maskSize } = useMaskSettings()
     useGSAP(() => {
-        gsap.set('.mask-wrapper',{
+        gsap.set('.mask-wrapper', {
             maskPosition: initialMaskPos,
             maskSize: initialMaskSize
         });
 
-        gsap.set('.mask-logo',{
+        gsap.set('.mask-logo', {
             marginTop: '-100vh',
             opacity: 0
         });
 
-        gsap.set('entrance-message',{
+        gsap.set('entrance-message', {
             marginTop: '0vh'
         });
 
         const tl = gsap.timeline({
             scrollTrigger: {
                 trigger: '.hero-section',
-                start: 'top top',               
+                start: 'top top',
                 scrub: 2.5,
                 end: '+=200%',
                 pin: true,
@@ -33,14 +33,16 @@ const Hero = () => {
         })
 
         tl
-        .to('.fade-out', {opacity: 0, ease: 'power1.inOut'})
-        .to('.scale-out', {scale: 1, ease: 'power1.inOut'})
-        .to('.mask-wrapper', {maskSize, ease: 'power1.inOut'}, '<')
-        .to('.mask-wrapper', {opacity: 0})
-        .to('.overlay-logo', {opacity: 1, onComplete: () => {
-            gsap.to('.overlay-logo', {opacity: 0})
-        }}, '<')
-        .to('.entrance-message', {duration: 1, ease: 'power1.inOut', maskImage: 'radial-gradient(circle at 50% 0vh, black 50%, transparent 100%)'}, '<')
+            .to('.fade-out', { opacity: 0, ease: 'power1.inOut' })
+            .to('.scale-out', { scale: 1, ease: 'power1.inOut' })
+            .to('.mask-wrapper', { maskSize, ease: 'power1.inOut' }, '<')
+            .to('.mask-wrapper', { opacity: 0 })
+            .to('.overlay-logo', {
+                opacity: 1, onComplete: () => {
+                    gsap.to('.overlay-logo', { opacity: 0 })
+                }
+            }, '<')
+            .to('.entrance-message', { duration: 1, ease: 'power1.inOut', maskImage: 'radial-gradient(circle at 50% 0vh, black 50%, transparent 100%)' }, '<')
     })
 
     return (
